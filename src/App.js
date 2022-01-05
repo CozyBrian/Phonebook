114a57072fab77c9625a1275c64f0e149bb25bd1
CozyBrian <dragonbrian33@gmail.com>
2022-01-05 02:37:36 +0000

    Error Checking and Routing

--- src/App.js
+++ src/App.js
@@ -2,19 +2,30 @@ import Header from './components/layout/header.component';
 import Contacts from './components/contacts/contacts.component';
 import AddContact from './components/contacts/add-contacts.component';
 import ContextProvider from './components/services/contacts/contacts.context';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <ContextProvider>
-      <div className="App">
-        <Header branding="Phone Book"/>
-        <div className="container">
-          <AddContact />
-          <Contacts />
+      <Router>
+        <div className="App">
+          <Header branding="Phone Book"/>
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Contacts/>} />
+              <Route exact path="/contacts/add" element={<AddContact/>} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </Router>
     </ContextProvider>
   );
 }
--- src/components/contacts/add-contacts.component.js
+++ src/components/contacts/add-contacts.component.js
@@ -1,15 +1,27 @@
 import React,{useState, useContext} from "react";
 import {v4 as uuidv4} from "uuid";
+import classnames from "classnames";
 import { ContactContext } from "../services/contacts/contacts.context";
 
 const AddContact = () => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
+  const [error, setError] = useState({});
 
   const {onAdd} = useContext(ContactContext);
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    if (name === "") {
+      setError({name:"Name is Required!"});
+      return; 
+    }
+
+    if (phone === "") {
+      setError({phone:"Phone is required"});
+      return;
+    }
 
     const contact = {
       id: uuidv4(),
@@ -19,35 +31,38 @@ const AddContact = () => {
     console.log(contact);
     onAdd(contact);
 
-    setName(""); setPhone("");
+    setName(""); setPhone(""); setError({});
   }
 
   return (
     <div className="card mb-3">
       <div className="card-header">Add Contact</div>
         <div className="card-body">
-          <form onSubmit={(e) => onSubmit(e)}>
+          <form onSubmit={(e) => onSubmit(e)} className="needs-validation">
             <div className="form-group">
               <label htmlFor="name">Name</label>
               <input
                 type="text"
                 name="name"
-                className="form-control form-control-lg"
+                id="name"
+                className={classnames("form-control form-control-lg", {"is-invalid" : error.name})}
                 placeholder="Enter Name..."
                 value={name}
                 onChange={(name) => setName(name.target.value)}
                 />
+              <div className="invalid-feedback">Name is required</div>
             </div>
             <div className="form-group">
               <label htmlFor="name">Phone</label>
               <input
                 type="text"
                 name="phone"
-                className="form-control form-control-lg"
+                className={classnames("form-control form-control-lg", {"is-invalid" : error.phone})}
                 placeholder="Enter Phone..."
                 value={phone}
                 onChange={(phone) => setPhone(phone.target.value)}
                 />
+                <div className="invalid-feedback">Phone is required</div>
             </div>
             <input
               type="submit"
