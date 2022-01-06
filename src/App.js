4d171b60185515a375167888e41f004473f94ab6
CozyBrian <dragonbrian33@gmail.com>
2022-01-06 00:29:34 +0000

    added edit and not found pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Header from "./components/layout/header.component";
 import Contacts from "./components/contacts/contacts.component";
 import AddContact from "./components/contacts/add-contacts.component";
+import EditContact from "./components/contacts/edit-contacts.component";
+import NotFound from "./components/pages/NotFound";
 import ContextProvider from "./components/services/contacts/contacts.context";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -17,6 +19,12 @@ function App() {
             <Routes>
               <Route exact path="/" element={<Contacts />} />
               <Route exact path="/contacts/add" element={<AddContact />} />
+              <Route
+                exact
+                path="/contacts/edit/:id"
+                element={<EditContact />}
+              />
+              <Route element={<NotFound />} />
             </Routes>
           </div>
         </div>
