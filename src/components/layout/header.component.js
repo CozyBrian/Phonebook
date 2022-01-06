54837dd2c3c34462958b0d82d4b09a2879d6a39b
CozyBrian <dragonbrian33@gmail.com>
2022-01-06 00:29:02 +0000

    minor changes

--- src/components/layout/header.component.js
+++ src/components/layout/header.component.js
@@ -1,21 +1,29 @@
+import { Link } from "react-router-dom";
 
-
-const Header = ({branding}) => {
-    return (
-      <nav className="nav navbar navbar-expand-sm navbar-dark bg-primary mb-3 py-0">
-        <div className='container'>
-        <a href="/" className="navbar-brand">{branding}</a>
-          <div>
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <a href='/' className='nav-link'>Home</a>
-              </li>
-            </ul>
-          </div>
+const Header = ({ branding }) => {
+  return (
+    <nav className="nav navbar navbar-expand-sm navbar-dark bg-primary mb-3 py-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          {branding}
+        </a>
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                <i className="fas fa-home" /> Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contacts/add" className="nav-link">
+                <i className="fas fa-plus" /> Add
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-    );
+      </div>
+    </nav>
+  );
 };
-
 
 export default Header;
