1ba3bddfb2b9d4b9ae440202253a060c81d28e19
CozyBrian <dragonbrian33@gmail.com>
2022-01-06 00:26:31 +0000

    contact component

--- src/components/contacts/contact.component.js
+++ src/components/contacts/contact.component.js
@@ -1,24 +1,44 @@
-import React,{useState, useContext} from "react";
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
 import { ContactContext } from "../services/contacts/contacts.context";
 
-export const Contact = ({contact}) => {
+export const Contact = ({ contact }) => {
   const [showInfo, setShowInfo] = useState(false);
-  const {onDelete} = useContext(ContactContext)
+  const { onDelete } = useContext(ContactContext);
 
   return (
-    <div className='card card-body mb-3'>
-      <h4>{contact.name}
-        <i onClick={() => setShowInfo(!showInfo)} className="fas fa-sort-down" style={{cursor: "pointer", paddingLeft: 6}}/>
-        <i onClick={() => onDelete(contact.id)} className="fas fa-times" style={{cursor: "pointer", float: "right", color: "red"}}/>
+    <div className="card card-body mb-3">
+      <h4>
+        {contact.name}
+        <i
+          onClick={() => setShowInfo(!showInfo)}
+          className="fas fa-sort-down"
+          style={{ cursor: "pointer", paddingLeft: 6 }}
+        />
+        <i
+          onClick={() => onDelete(contact.id)}
+          className="fas fa-times"
+          style={{ cursor: "pointer", float: "right", color: "red" }}
+        />
+        <Link to={`contacts/edit/${contact.id}`}>
+          <i
+            className="fas fa-pencil-alt"
+            style={{
+              cursor: "pointer",
+              float: "right",
+              color: "black",
+              paddingRight: 4,
+            }}
+          />
+        </Link>
       </h4>
       {showInfo ? (
-        <ul className='list-group'>
-          <li className='list-group-item'>Phone: {contact.phone}</li>
+        <ul className="list-group">
+          <li className="list-group-item">Phone: {contact.phone}</li>
         </ul>
-        )  : null
-      }
+      ) : null}
     </div>
-  )
-}
+  );
+};
 
 export default Contact;
